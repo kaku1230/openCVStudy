061915a23dc8c959e1683e5655505aa25bfaadb4
kaku <812814843@qq.com>
2019-07-13 17:35:16 +0800

    first commit

---
+++ videoanalysis/BackgroundSubtractionMethods.py
@@ -0,0 +1,19 @@
+import numpy as np
+import cv2 as cv
+
+cap = cv.VideoCapture('vtest.avi')
+fgbg = cv.bgsegm.createBackgroundSubtractorMOG()
+fgbg = cv.createBackgroundSubtractorMOG2()
+kernel = cv.getStructuringElement(cv.MORPH_ELLIPSE,(3,3))
+fgbg = cv.bgsegm.createBackgroundSubtractorGMG()
+
+while(1):
+    ret, frame = cap.read()
+    fgmask = fgbg.apply(frame)
+    cv.imshow('frame',fgmask)
+    k = cv.waitKey(30) & 0xff
+    if k == 27:
+        break
+
+cap.release()
+cv.destroyAllWindows()
